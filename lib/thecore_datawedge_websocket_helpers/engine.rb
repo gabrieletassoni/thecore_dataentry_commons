ff579c371f929a4559b2591d2b3d50f7afed2003
Gabriele Tassoni <gabriele.tassoni@gmail.com>
2017-09-01 10:10:59 +0200

    debug

--- lib/thecore_datawedge_websocket_helpers/engine.rb
+++ lib/thecore_datawedge_websocket_helpers/engine.rb
@@ -1,4 +1,13 @@
 module ThecoreDatawedgeWebsocketHelpers
   class Engine < ::Rails::Engine
+
+    initializer 'chs_models.add_to_migrations' do |app|
+      unless app.root.to_s == root.to_s
+        # APPEND TO MAIN APP MIGRATIONS FROM THIS GEM
+        config.paths['db/migrate'].expanded.each do |expanded_path|
+          app.config.paths['db/migrate'] << expanded_path
+        end
+      end
+    end
   end
 end
