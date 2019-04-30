3b364230924f93bcba99c4724bc3f9a0809524b2
Gabriele Tassoni <gabriele.tassoni@gmail.com>
2019-04-30 16:29:48 +0200

    migration initializer for engine cleanup

--- lib/thecore_dataentry_commons/engine.rb
+++ lib/thecore_dataentry_commons/engine.rb
@@ -8,11 +8,13 @@ module ThecoreDataentryCommons
     #   app.middleware.use ::ActionDispatch::Static, "#{root}/app"
     # end
     initializer 'thecore_dataentry_commons.add_to_migrations' do |app|
-      unless app.root.to_s == root.to_s
+      unless app.root.to_s.match root.to_s
         # APPEND TO MAIN APP MIGRATIONS FROM THIS GEM
         config.paths['db/migrate'].expanded.each do |expanded_path|
           app.config.paths['db/migrate'] << expanded_path
         end
+        #  = app.config.paths['db/migrate'].uniq
+        # puts app.config.paths['db/migrate'].map(:path).inspect
       end
     end
   end
